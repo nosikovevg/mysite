e26bf01df1f107b951659b62ba205b132cc5fed9
Nos <zvezdazanatih@gmail.com>
2025-02-10 09:37:17 +0700

    js6

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const inputNode = document.querySelector(".js-input");
 const btnNode = document.querySelector(".js-btn");
 const ocNode = document.querySelector(".js-oc");
 
+let title = '';
+
+function changeTitle(newTitle) {
+    title = newTitle;
+}
+
+function render() {
+    ocNode.innerHTML = `<h1> ${title} </h1>`;
+}
 btnNode.addEventListener("click", function() {
-    console.log(inputNode.value);
-    ocNode.innerHTML = <h1> ${inputNode.value} </h1>;
+    changeTitle(inputNode.value);
+    render();
+
 })
 
 
