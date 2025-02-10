971756a61527bfe24791a498c9012113f483cc59
Nos <zvezdazanatih@gmail.com>
2025-02-10 09:58:59 +0700

    js7

--- index.js
+++ index.js
@@ -3,17 +3,29 @@ const inputNode = document.querySelector(".js-input");
 const btnNode = document.querySelector(".js-btn");
 const ocNode = document.querySelector(".js-oc");
 
-let title = '';
 
-function changeTitle(newTitle) {
-    title = newTitle;
+// состояние
+let state = {
+    title: ''
 }
+
+// изменение состояния
+function changeState(newState) {
+    state = {...newState};
+}
+
+// рендер
 
 function render() {
-    ocNode.innerHTML = `<h1> ${title} </h1>`;
+    ocNode.innerHTML = `<h1> ${state.title} </h1>`;
 }
+
+
+// контроллер
 btnNode.addEventListener("click", function() {
-    changeTitle(inputNode.value);
+    changeState({
+        title: inputNode.value
+});
     render();
 
 })
